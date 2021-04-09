553d79bbfc00fab4e61d3102ca54fb070951a106
qeqeq1 <37824806+qeqeq1@users.noreply.github.com>
2021-04-10 01:25:21 +0800

    Add files via upload

---
+++ paste.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+if [[ -z "$(command -v curl)" ]];then
+    echo "没有curl，请安装"
+exit 1
+fi
+content=$*
+if [[ "$content" ]];then
+    share_link="https://paste.ubuntu.com"$(curl -v --data-urlencode "content=${content}" -d "poster=napoi.cn" -d "syntax=text" "https://paste.ubuntu.com" 2>&1 | grep "Location" | awk '{print $3}')
+    echo $share_link
+else
+    echo "没有传入要上传的文本"
+    echo -e "一般是这样：\e[31mbash $0 \e[32m你要上传的文本\e[0m"
+fi
